a7e4f5664dc446020e7301d135c12b88700494e0
rykyou <rkim1214@gmail.com>
2018-12-06 17:42:36 -0500

    finalize sort_list method

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,15 +1,9 @@
-require 'pry'
-
 class Cli
-
   #place where we write methods relating to interface of our app
+  attr_writer :user_list
 
   def user_list
     @user_list ||= []
-  end
-
-  def user_sorted_hash
-    @user_sorted_hash ||= Hash.new(0)
   end
 
   def start
@@ -17,7 +11,6 @@ class Cli
     puts "After each item, hit 'return/enter' before proceeding on to the next item."
     puts "Type in 'done' when you are finished and would like to see your completed list."
     puts "Type 'exit' when you would like to leave the app."
-    # puts Item.first.name
     self.get_user_input
   end
 
@@ -25,15 +18,10 @@ class Cli
     user_response = gets.strip.downcase
     if user_response == "done"
       self.user_done
-      # self.sort_list(self.user_list)
-      # puts "Here is your list:"
-      # user_list.collect {|user_item| puts user_item.name }
-      #
-
     elsif user_response == "exit"
       abort
     else
-      current_item = Item.find_by(name: "#{user_response}")
+      current_item = Item.find_by(name: user_response)
       if current_item
         self.user_list << current_item
         self.get_user_input
@@ -44,30 +32,39 @@ class Cli
     end
   end
 
-  def sort_list(array)
-    new_array = array.collect {|item_obj| item_obj.stores }
-    new_array.each do |stores_for_an_item|
-      stores_for_an_item.each { |store| self.user_sorted_hash[store] += 1 }
+  def sort_list(item_list)
+    #return an array of arrays that looks like [store_obj, [item_obj, item_obj]], [store_obj], [item_obj, item_obj]]
+    # byebug
+    stores_items_hash = {}
+    item_list.each do |item_obj|
+      item_obj.stores.each do |store_obj|
+        if stores_items_hash[store_obj]
+          stores_items_hash[store_obj] << item_obj
+        else
+          stores_items_hash[store_obj] = []
+          stores_items_hash[store_obj] << item_obj
+        end
+      end
     end
-    self.user_sorted_hash
+    self.user_list = stores_items_hash.sort_by{|k,v| v.length}.reverse
   end
 
   def user_done
     self.sort_list(self.user_list)
-    priority_store = user_sorted_hash.key(user_sorted_hash.values.max)
-    puts "It looks like most of your items can be found at #{priority_store.name}"
-    puts "To see your list of items, choose a store by its number:"
+    puts "Here's our suggestions for stores that have the items you're looking for:"
     counter = 0
-    user_sorted_hash.each do |store, v|
+    self.user_list.each do |store, v|
       counter += 1
       puts "#{counter}. #{store.name}"
     end
-     # user_list.collect {|user_item| puts user_item.name }
+    self.list_of_store
   end
 
-
-
-
-
+  def list_of_store
+    puts "Enter number:"
+    user_input = gets.strip
+    x = self.sort_list_to_hash(self.user_list)[user_input]
+    binding.pry
+  end
 
 end
