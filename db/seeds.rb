4b5aa39eac568431d6651e60621381ea28fd8e64
github username <github email address>
2018-12-06 21:40:21 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-# Item.destroy_all
-# Store.destroy_all
+Item.destroy_all
+Store.destroy_all
 
 petco = Store.find_or_create_by(name: "petco")
 bestbuy = Store.find_or_create_by(name: "best buy")
@@ -11,10 +11,12 @@ target = Store.find_or_create_by(name: "target")
 milk = Item.find_or_create_by(name: "milk")
 organic_granola = Item.find_or_create_by(name: "organic granola")
 free_range_eggs = Item.find_or_create_by(name: "free range eggs")
+bread = Item.find_or_create_by(name: "bread")
 
 tv = Item.find_or_create_by(name: "tv")
 smart_watch = Item.find_or_create_by(name: "smart watch")
 phone_charger = Item.find_or_create_by(name: "phone charger")
+video_game = Item.find_or_create_by(name: "video game")
 
 football = Item.find_or_create_by(name: "football")
 golf_club = Item.find_or_create_by(name: "golf club")
@@ -28,22 +30,22 @@ dog_food = Item.find_or_create_by(name: "dog food")
 litterbox = Item.find_or_create_by(name: "litterbox")
 leash = Item.find_or_create_by(name: "leash")
 
-# [tv, smart_watch, phone_charger].each do |product|
-#   StoreItem.find_or_create_by(store: bestbuy, item: product)
-# end
-#
-# [dog_food, litterbox, leash].each do |product|
-#   StoreItem.create(store: petco, item: product)
-# end
-#
-# [milk, organic_granola, free_range_eggs, candy, soda, toothbrush, dog_food].each do |product|
-#   StoreItem.create(store: wholefoods, item: product)
-# end
-#
-# [football, golf_club, baseball, smart_watch].each do |product|
-#   StoreItem.create(store: dicks, item: product)
-# end
-#
-# [milk, tv, smart_watch, phone_charger, football, toothbrush, candy, soda, leash].each do |product|
-#   StoreItem.create(store: target, item: product)
-# end
+[video_game, tv, smart_watch, phone_charger].each do |product|
+  StoreItem.find_or_create_by(store: bestbuy, item: product)
+end
+
+[dog_food, litterbox, leash].each do |product|
+  StoreItem.create(store: petco, item: product)
+end
+
+[bread, milk, organic_granola, free_range_eggs, candy, soda, toothbrush, dog_food].each do |product|
+  StoreItem.create(store: wholefoods, item: product)
+end
+
+[football, golf_club, baseball, smart_watch].each do |product|
+  StoreItem.create(store: dicks, item: product)
+end
+
+[video_game, bread, milk, tv, smart_watch, phone_charger, football, toothbrush, candy, soda, leash].each do |product|
+  StoreItem.create(store: target, item: product)
+end
