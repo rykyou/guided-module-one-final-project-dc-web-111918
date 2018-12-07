2757fb951f212ca5dbb827aaaa959ca0e11776d6
rykyou <rkim1214@gmail.com>
2018-12-07 01:37:35 -0500

    stylize app

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,49 +1,75 @@
 class Cli
  #place where we write methods relating to interface of our app
- attr_writer :user_list
 
+  attr_writer :user_list
+
+#acts as reader method of @user_list if it is not empty. If empty, set @user_list to empty array
   def user_list
     @user_list ||= []
   end
 
-#greeting user and asking what they could like to do.
+#greeting user and asking what they would like to do
   def start
-    puts "Hello Shopper! List out what you need to buy and we'll help you figure out which store you need to go to."
     puts ""
-    puts "After each item, hit 'return/enter' before proceeding on to the next item."
     puts ""
-    puts "Type in 'done' when you are finished and would like to see your completed list."
+    puts <<-'EOF'
+
+           _    _      _ _          _____ _                                 _
+          | |  | |    | | |        / ____| |                               | |
+          | |__| | ___| | | ___   | (___ | |__   ___  _ __  _ __   ___ _ __| |
+          |  __  |/ _ \ | |/ _ \   \___ \| '_ \ / _ \| '_ \| '_ \ / _ \ '__| |
+          | |  | |  __/ | | (_) |  ____) | | | | (_) | |_) | |_) |  __/ |  |_|
+          |_|  |_|\___|_|_|\___/  |_____/|_| |_|\___/| .__/| .__/ \___|_|  (_)
+                                                     | |   | |
+                                                     |_|   |_|
+
+    EOF
+    puts "------------------------------------------------------------------------------------------"
+    puts "List out what you need to buy and we'll help you figure out which store you need to go to!"
     puts ""
-    puts "Type 'exit' when you would like to leave the app."
-    self.get_user_input
+    puts ""
+    puts "     _________________________________________________________________________________"
+    puts "    |After each item, hit 'return/enter' before proceeding on to the next item.       |"
+    puts "    |                                                                                 |"
+    puts "    |Type in 'done' when you are finished and would like to see your completed list.  |"
+    puts "    |                                                                                 |"
+    puts "    |Type 'exit' when you would like to leave the app.                                |"
+    puts "     ---------------------------------------------------------------------------------"
+    puts ""
+    puts ""
+    puts ""
+    puts "What do you need to get?"
+    puts "------------------------"
+    self.get_user_items
   end
 
 #responding to user's input and directing them where they want to go.
-  def get_user_input
+  def get_user_items
     user_response = gets.strip.downcase
+    puts ""
     if user_response == "done"
       self.sort_list(self.user_list)
-      self.user_done
+      self.suggestion_of_stores
     elsif user_response == "exit"
       abort
     else
       current_item = Item.find_by(name: user_response)
       if current_item
         self.user_list << current_item
-        self.get_user_input
+        self.get_user_items
       else
-        puts "Invalid item, please try again."
-        self.get_user_input
+        puts "<Invalid item, please try again.>"
+        puts ""
+        self.get_user_items
       end
     end
   end
 
-#
-  def sort_list(item_list)
-    #return an array of arrays that looks like [store_obj, [item_obj, item_obj]], [store_obj], [item_obj, item_obj]]
-    # byebug
+#reconstructs array of item objects into new array that looks like [store_obj, [item_obj, item_obj]], [store_obj], [item_obj]]
+# @user_list is reset to be this new array so it can be used as oragnized collection that can be indexed
+  def sort_list(items_array)
     stores_items_hash = {}
-    item_list.each do |item_obj|
+    items_array.each do |item_obj|
       item_obj.stores.each do |store_obj|
         if stores_items_hash[store_obj]
           stores_items_hash[store_obj] << item_obj
@@ -56,30 +82,39 @@ class Cli
     self.user_list = stores_items_hash.sort_by{|k,v| v.length}.reverse
   end
 
-  def user_done
-    puts "Here's our suggestions for stores that have the item(s) you're looking for:"
+  def suggestion_of_stores
+    puts ""
+    puts ""
+    puts ""
+    puts ""
+    puts "Here's our suggestion of stores that have the item(s) you're looking for"
+    puts "ordered so the stores at the top of the list have more of the items you need:"
+    puts "--------------------------------------------------------------------------------"
     counter = 0
     self.user_list.each do |store, v|
       counter += 1
       puts "#{counter}. #{store.name.upcase}"
     end
-    self.list_of_store
+    puts ""
+    puts "Enter number to see each store's list:"
+    self.list_for_chosen_store
   end
 
-  def list_of_store
-    puts "Enter number to see each store's list:"
+  def list_for_chosen_store
     user_input = gets.strip.to_i
-    if user_input == 0
-      puts "Must be a number on the list. Please try again."
-      self.list_of_store
-    elsif user_input > self.user_list.size
-      puts "Must be a number on the list. Please try again."
-
-      self.list_of_store
+    if user_input == 0 || user_input > self.user_list.size
+      puts ""
+      puts "<Must be a number on the list. Please try again.>"
+      self.list_for_chosen_store
     else
       user_input -= 1
       current_store = self.user_list[user_input][0].name
       items_array = self.user_list[user_input][1]
+      puts ""
+      puts ""
+      puts ""
+      puts ""
+      puts ""
       puts "#{current_store.upcase}:"
       puts "-----------------------"
       #displays list of items under store
@@ -90,29 +125,30 @@ class Cli
       end
       # puts ""
     end
-    self.back_to_stores
+    self.options
   end
 
 
-  def back_to_stores
-    puts "Type 'back' to go back to the list of stores, 'menu' to go back to main menu of app or press 'exit' to exit app."
+  def options
+    puts " __________________________________________________"
+    puts "|Type one of the following:                        |"
+    puts "|  'back' to go back to the list of stores         |"
+    puts "|  'menu' to go back to create a new shopping list |"
+    puts "|  'exit' to exit the app                          |"
+    puts " --------------------------------------------------"
     user_input = gets.strip
     if user_input == "back"
-      self.user_done
-      # counter = 0
-      # self.user_list.each do |store, v|
-      #   counter += 1
-      #   puts "#{counter}. #{store.name.upcase}"
-      #   self.list_of_store
+      self.suggestion_of_stores
     elsif user_input == "menu"
       self.user_list = []
       self.start
-    else
+    elsif user_input == "exit"
       abort
+    else
+      puts ""
+      puts "<Invalid option. Try again!>"
+      self.options
     end
   end
 
 end
-
-#picks out best store for user to go to:
-# new_hash = hash.key(hash.values.max)
